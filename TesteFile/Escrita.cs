dd8975c899494b5f2a545dd1392ab673ac6f4707
monike stephany santana <monikestephany@outlook.com>
2020-03-05 14:58:48 -0300

    header

--- TesteFile/Escrita.cs
+++ TesteFile/Escrita.cs
@@ -11,10 +11,13 @@ namespace TesteFile
         {
             List<string> outPut = new List<string>();
 
+            int cont = 1;
             foreach (var f in input)
             {
                 string linha = "";
                 linha = linha.PadRight(909, ' ');
+                string header = "";
+                header = header.PadRight(909, ' ');
                 for (int i = 0; i < models.Count; i++)
                 {
                     foreach (var item in f.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
@@ -22,12 +25,24 @@ namespace TesteFile
                         var sp = models[i].Split(';');
                         if (item.Name == sp[2])
                         {
-                            linha = linha.Insert(int.Parse(sp[0]), item.GetValue(f).ToString().SetLengthString(int.Parse(sp[1])));
-                          
+                            if (sp[3] == "H" && cont == 1)
+                            {
+                                header = header.Insert(int.Parse(sp[0]), item.GetValue(f).ToString().SetLengthString(int.Parse(sp[1])));
+                              
+                            }
+                            else
+                            {
+                                linha = linha.Insert(int.Parse(sp[0]), item.GetValue(f).ToString().SetLengthString(int.Parse(sp[1])));
+                            }                         
                         }
                     }
                 }
+                if (cont == 1)
+                {
+                    outPut.Add(header);
+                }
                 outPut.Add(linha);
+                cont++;
             }
             return outPut;
         }     
